00af49a578c38f643542e88d3e30b5f150d16038
jovian153 <pieterjovian@gmail.com>
2026-03-09 12:21:33 +0800

    Second commit

---
+++ src/Polymorphism_soal_2/Poly62.java
@@ -0,0 +1,22 @@
+public class Poly62 {
+    public static void main(String[] args){
+        Vehicle v1 = new Vehicle();
+        Vehicle v2 = new Car();
+
+        v1.move();
+        v2.move();
+    }
+}
+
+class Vehicle {
+    void move() {
+    System.out.println("Vehicle is moving");
+    }
+}
+
+class Car extends Vehicle {
+@Override
+void move() {
+    System.out.println("Car is moving");
+    }
+}
